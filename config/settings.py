9f3ec452405d5f907ba9aeb922a7833a5e0acc95
Stancy-ai <seyramtheresa@gmail.com>
2022-12-09 01:11:07 +0000

    new updates fro heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9kje@yy9llel7eatz#6ssz6#snn)tjcx&av+g7cds%2&h@zyt#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
